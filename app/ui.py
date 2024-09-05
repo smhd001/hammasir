44f44214b8c471707c7f2e40dbd174eceab50523
hammasirpc <hammasirpc@gmail.com>
2024-09-05 19:01:25 +0330

    fix bugs in ui add text filed

--- app/ui.py
+++ app/ui.py
@@ -3,7 +3,7 @@ import streamlit as st
 from elastic_query import search
 from streamlit_mic_recorder import speech_to_text
 
-# from slot_filing import slot_filing
+from slot_filing import slot_filing
 
 state = st.session_state
 
@@ -30,18 +30,24 @@ summary, input, .stMarkdown {
 )
 
 
-text = speech_to_text(
-    language="fa", use_container_width=True, just_once=True, key="STT"
-)
-text = "دکتر قلب خوش اخلاق وکیل آباد مشهد"
+voice_input, text_input = st.tabs(["voice", "text"])
+
+with voice_input:
+    text = speech_to_text(
+        language="fa", use_container_width=True, just_once=True, key="STT"
+    )
+with text_input:
+    text = st.text_input("search")
+
+# text = "دکتر قلب خوش اخلاق وکیل آباد مشهد"
 
 if text:
     state.text_received = text
     print("result of asr", text)
-    # slots = slot_filing(text)
-    # print("slotl", slots)
-    # state.slots = slots
-    search_result = search("")
+    slots = slot_filing(text)
+    print("slotl", slots)
+    state.slots = slots
+    search_result = search(slots)
     print("search", search_result)
     state.search_result = search_result
 
@@ -52,23 +58,19 @@ st.html('<p style="direction:LTR">slots<p>')
 state.slots
 
 for res in state.search_result:
-    title, url, description = res
+    title, lat_long, description = res
     with st.expander(title):
-        json_view, site_view = st.tabs(["json", "site"])
+        json_view, info_view = st.tabs(["json", "info"])
         with json_view:
             description
-        with site_view:
+        with info_view:
             col1, col2 = st.columns(2)
             with col1:
                 st.image(config.P24_BASE_IMAGE_URL + description["image"])
             with col2:
                 p24_url = config.p24_BASE_URL + description["url"]
                 f"[مشاهده در پزشک 24]({p24_url})"
-                neshan_url = (
-                    config.NESHAN_BASE_URL
-                    + "/"
-                    + description["lat"]
-                    + ","
-                    + description["lon"]
-                )
-                f"[مسیر یابی در نشان]({neshan_url})"
+                if lat_long:
+                    lat, long = lat_long
+                    neshan_url = config.NESHAN_BASE_URL + "/" + str(lat) + "," + str(long)
+                    f"[مسیر یابی در نشان]({neshan_url})"
