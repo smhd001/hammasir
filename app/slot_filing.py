9189a1c72468620dda4f2021f5fd5aa30855e05b
hammasirpc <hammasirpc@gmail.com>
2024-09-05 19:00:59 +0330

    change excution provider from Tensorrt to CUADExecuttion as it's faster fix model path

--- app/slot_filing.py
+++ app/slot_filing.py
@@ -1,18 +1,22 @@
+import os
 from optimum.onnxruntime import ORTModelForTokenClassification
 from transformers import AutoTokenizer, pipeline
 
-model = ORTModelForTokenClassification.from_pretrained(
-    "../slot_filling/traininng/ner_model/checkpoint-77",
-    export=True,
-    provider="TensorrtExecutionProvider",
-    from_transformers=True,
-)
+# set model path if we are runnig in docker or no
+if "DOCKER_CONTAINER" in os.environ:
+    model_path = "/model"
+else:
+    model_path = "../slot_filling/traininng/ner_model/checkpoint-77"
 
+model = ORTModelForTokenClassification.from_pretrained(
+    model_path,
+    export=True,
+    provider="CUDAExecutionProvider",
+)
 
 model_id = "HooshvareLab/bert-base-parsbert-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 classifier = pipeline("ner", model=model, tokenizer=tokenizer, device="cuda")
-
 
 def slot_filing(text) -> dict[str, str]:
     output = classifier(text)
