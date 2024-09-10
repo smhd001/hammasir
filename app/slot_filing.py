fe35ad207e0d8c181c11446aca720af7e28e7d51
mohammad hashemy <smhd.hashemy@outlook.com>
2024-09-10 13:54:29 +0330

    Revert "use aria-bert instead of bert"
    This reverts commit 47ec16fd0864e0d39a943e1e8633399f5e742a1e.

--- app/slot_filing.py
+++ app/slot_filing.py
@@ -7,7 +7,7 @@ from transformers import AutoTokenizer, pipeline
 if "DOCKER_CONTAINER" in os.environ:
     model_path = "/model"
 else:
-    model_path = "/home/ham/mlartifacts/867072811883705549/b23eda71b69548899cd3666c499206c3/artifacts/checkpoint-255/artifacts/checkpoint-255"
+    model_path = "../slot_filling/training/sf_model/checkpoint-112"
 
 model = ORTModelForTokenClassification.from_pretrained(
     model_path,
@@ -15,7 +15,7 @@ model = ORTModelForTokenClassification.from_pretrained(
     provider="CUDAExecutionProvider",
 )
 
-model_id = "ViraIntelligentDataMining/AriaBERT"
+model_id = "HooshvareLab/bert-base-parsbert-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 classifier = pipeline("ner", model=model, tokenizer=tokenizer, device="cuda")
 
@@ -25,25 +25,17 @@ def slot_filing(text) -> dict[str, str]:
     result = {}
     word = ""
     last_tag = None
-    last_end = 0
     for en in output:
         boi, tag = en["entity"].split("-", 1)
         if boi == "B":
             if word:
                 result.setdefault(last_tag, []).append(word.strip())
-            word = text[en["start"]:en["end"]]
-            if en["start"] != last_end:
-                word += " "
+            word = en["word"] + " "
         elif boi == "I":
-            if en["start"] != last_end:
-                word += " "
-            word += text[en["start"]:en["end"]] + " "
+            word += en["word"] + " "
         else:
             raise ValueError("tags most start with B or I")
         last_tag = tag
-        last_end = en["end"]
     if word:
         result.setdefault(last_tag, []).append(word.strip())
     return result
-
-print(slot_filing("سرم درد می کنه یکی می خوام تو مشهد"))
